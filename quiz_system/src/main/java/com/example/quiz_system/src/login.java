6648eb5bf011ab31270fd4939d9fa41fb51746d4
NT2602 <ngocthanh6633@gmail.com>
2026-04-03 21:06:12 +0700

    fix-register

--- quiz_system/src/main/java/com/example/quiz_system/src/login.java
+++ quiz_system/src/main/java/com/example/quiz_system/src/login.java
@@ -24,9 +24,8 @@ public class login extends JFrame implements ActionListener{
         heading.setFont(new Font("Viner Hand ITC", Font.BOLD, 40));
         heading.setForeground(new Color(30, 144, 254));
         add(heading);
-        
-        JLabel name = new JLabel("Enter your name hello");
-        JLabel name = new JLabel("Enter what your name here:");
+    
+        JLabel name = new JLabel("Enter your name1");
         name.setBounds(810, 150, 300, 20);
         name.setFont(new Font("Mongolian Baiti", Font.BOLD, 18));
         name.setForeground(new Color(30, 144, 254));
