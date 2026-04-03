e447e4b05e4bb0b5c32c689a9d603edb03fed2d8
phuoc-hai-2 <daophuochai2004@gmail.com>
2026-04-03 18:13:45 +0700

    tạo môi trường

---
+++ quiz_system/src/test/java/com/example/quiz_system/QuizSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quiz_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
